5fcf632f6ae57b07c8b023fb921751d44a3ee4f6
nakyeong <rinarina0429@naver.com>
2025-02-18 17:07:09 +0900

    [Chore] @EnableJpaAuditing

--- src/main/java/com/GDGoC/BaS/BaSApplication.java
+++ src/main/java/com/GDGoC/BaS/BaSApplication.java
@@ -2,12 +2,14 @@ package com.GDGoC.BaS;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class BaSApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BaSApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(BaSApplication.class, args);
+    }
 
 }
