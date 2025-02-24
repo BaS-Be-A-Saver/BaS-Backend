42638f3743e6998d808296b9c86b906daed52c39
nakyeong <rinarina0429@naver.com>
2025-02-24 20:03:33 +0900

    [Feat] JwtTokenProvider 추가

---
+++ src/main/java/com/GDGoC/BaS/config/JwtTokenProvider.java
@@ -0,0 +1,31 @@
+package com.GDGoC.BaS.config;
+
+import com.GDGoC.BaS.user.User;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import java.util.Date;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+public class JwtTokenProvider {
+
+    @Value("${jwt.secret}")
+    private String jwtSecret;
+
+    @Value("${jwt.expiration}")
+    private long expiration;
+
+    public String generateToken(User user) {
+        Date now = new Date();
+        Date expiryDate = new Date(now.getTime() + expiration);
+
+        return Jwts.builder()
+                .setSubject(Long.toString(user.getUserId()))
+                .claim("email", user.getEmail())
+                .setIssuedAt(now)
+                .setExpiration(expiryDate)
+                .signWith(SignatureAlgorithm.HS512, jwtSecret)
+                .compact();
+    }
+}
