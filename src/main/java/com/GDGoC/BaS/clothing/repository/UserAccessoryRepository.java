9ea09db793f5b83d11f9c13930fbea5789b5bff0
rinarina0429 <forrina0429@gmail.com>
2025-03-30 00:39:36 +0900

    [Feat] 옷장 조회 위한 findAllByUser() 추가

--- src/main/java/com/GDGoC/BaS/clothing/repository/UserAccessoryRepository.java
+++ src/main/java/com/GDGoC/BaS/clothing/repository/UserAccessoryRepository.java
@@ -2,6 +2,7 @@ package com.GDGoC.BaS.clothing.repository;
 
 import com.GDGoC.BaS.clothing.domain.UserAccessory;
 import com.GDGoC.BaS.user.domain.User;
+import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -10,4 +11,6 @@ import org.springframework.stereotype.Repository;
 public interface UserAccessoryRepository extends JpaRepository<UserAccessory, Long> {
 
     Optional<UserAccessory> findByUserAndIsEquippedTrue(User user);
+
+    List<UserAccessory> findAllByUser(User user);
 }
