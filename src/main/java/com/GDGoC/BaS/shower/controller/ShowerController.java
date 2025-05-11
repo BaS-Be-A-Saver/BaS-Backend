6dc07b4dab7d95fa9f16c71cef88ce700b87000d
rinarina0429 <forrina0429@gmail.com>
2025-05-11 20:48:58 +0900

    [Rename] 샤워시간 기록 uri 수정

--- src/main/java/com/GDGoC/BaS/shower/controller/ShowerController.java
+++ src/main/java/com/GDGoC/BaS/shower/controller/ShowerController.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/shower")
+@RequestMapping("/showers")
 @RequiredArgsConstructor
 public class ShowerController {
 
