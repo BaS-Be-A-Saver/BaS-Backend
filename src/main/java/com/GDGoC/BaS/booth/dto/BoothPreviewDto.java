b0caf634a8d4aa4764b18211889721fdba08d03e
rinarina0429 <forrina0429@gmail.com>
2025-05-14 15:29:24 +0900

    [Feat] 부스 목록 조회 dto

---
+++ src/main/java/com/GDGoC/BaS/booth/dto/BoothPreviewDto.java
@@ -0,0 +1,8 @@
+package com.GDGoC.BaS.booth.dto;
+
+public record BoothPreviewDto(
+        Long boothId,
+        String name,
+        boolean successful
+) {
+}
---
+++ src/main/java/com/GDGoC/BaS/booth/dto/MyBoothsDto.java
@@ -0,0 +1,8 @@
+package com.GDGoC.BaS.booth.dto;
+
+import java.util.List;
+
+public record MyBoothsDto(
+        List<BoothPreviewDto> booths
+) {
+}
