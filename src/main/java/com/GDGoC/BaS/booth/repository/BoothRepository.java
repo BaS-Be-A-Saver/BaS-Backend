d52158d36f58e8393508cd32ea7793117df9d393
rinarina0429 <forrina0429@gmail.com>
2025-05-14 16:17:30 +0900

    [Feat] 부스 목록 조회 위한 레포지토리 코드 추가

--- src/main/java/com/GDGoC/BaS/booth/repository/BoothRepository.java
+++ src/main/java/com/GDGoC/BaS/booth/repository/BoothRepository.java
@@ -1,12 +1,19 @@
 package com.GDGoC.BaS.booth.repository;
 
 import com.GDGoC.BaS.booth.domain.Booth;
+import com.GDGoC.BaS.user.domain.User;
+import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface BoothRepository extends JpaRepository<Booth, Long> {
 
     Optional<Booth> findByCode(String code);
+
+    @Query("SELECT bu.booth FROM BoothUser bu WHERE bu.user = :user")
+    List<Booth> findBoothsByUser(@Param("user") User user);
 }
